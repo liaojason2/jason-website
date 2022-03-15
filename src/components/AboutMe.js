df7fffefa9d30345415b95532298ef334be5da0e
Jason Liao <liao.jason2@gmail.com>
2022-03-15 22:39:57 +0800

    Add About Me

---
+++ src/components/AboutMe.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+function AboutMe(props){
+  return(
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <h1>About Me</h1>
+          <p>
+            I am a software engineer with a passion for learning and problem solving.
+            I have a background in mathematics and computer science and I am currently
+            pursuing a B.S. in Computer Science at the University of California,
+            Irvine. I am currently working at <a href="https://www.linkedin.com/company/google/">Google</a>
+            as a software engineer.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+export default AboutMe;
