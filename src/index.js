f5d74cd8034859d7991faabfe68ff5b741ad5772
Jason Liao <liao.jason2@gmail.com>
2022-03-15 22:39:41 +0800

    Modify Root View

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, {Component }from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap-social';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -15,3 +18,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
